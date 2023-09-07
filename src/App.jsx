c014899f519861c9d4d85b037972bbe163bfe0c6
Ahmad Bilal Farooqi <ahmadbilal20152016@gmail.com>
2023-09-07 14:59:15 +0500

    updated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl bg-orange-600">Currency App</h1>
+    </>
+  );
+}
+
+export default App;
